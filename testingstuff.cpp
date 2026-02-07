ee3412e0c38e231c0b4c9872f21bc9243b080965
Qihong Liang <philipliang6@gmail.com>
2026-02-07 16:47:54 -0500

    just testing stuff

---
+++ testingstuff.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include <string>
+
+using namespace std; 
+
+/* this is a multi line comment 
+that includes 2+ lines */
+
+int main() {
+   int countDown = 3; 
+
+   while (countDown > 0) {
+    cout << countDown << endl; 
+    countDown--; 
+   }
+   cout << "happy new year" << endl; 
+    
+   
+}
